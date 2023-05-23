2ce0021163057e95f48aca0396aee8921dc1727e
Hsu <myathsu703@gmail.com>
2023-05-23 14:56:56 +0630

    0.8

---
+++ build/precache-manifest.e669a784bd6e579860638009faf04edf.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "c46cb89afc14146d018d",
+    "url": "/static/css/main.d325fb5e.chunk.css"
+  },
+  {
+    "revision": "c46cb89afc14146d018d",
+    "url": "/static/js/main.c46cb89a.chunk.js"
+  },
+  {
+    "revision": "b294daaa80292132610c",
+    "url": "/static/js/1.b294daaa.chunk.js"
+  },
+  {
+    "revision": "608c95c66e50bdbb853a",
+    "url": "/static/css/2.ebd86d51.chunk.css"
+  },
+  {
+    "revision": "608c95c66e50bdbb853a",
+    "url": "/static/js/2.608c95c6.chunk.js"
+  },
+  {
+    "revision": "96105c23258367a06e58",
+    "url": "/static/js/runtime~main.96105c23.js"
+  },
+  {
+    "revision": "7ce860d72108ab2469bf95c3cdd0c74c",
+    "url": "/static/media/BritannicDRegular.7ce860d7.ttf"
+  },
+  {
+    "revision": "481816ebe7b2dc627596ef2fd9507f0b",
+    "url": "/static/media/Arialn.481816eb.ttf"
+  },
+  {
+    "revision": "2bf40ab72ea4897a3fd9b6e48b233a19",
+    "url": "/static/media/Garamond Regular.2bf40ab7.ttf"
+  },
+  {
+    "revision": "c0e1dc37d2d4a02f0fabb25aac72d6db",
+    "url": "/static/media/LucidaUnicodeCalligraphy.c0e1dc37.ttf"
+  },
+  {
+    "revision": "a8006b37472df2e365d439d4ec1f48bf",
+    "url": "/static/media/eb-garamond.12-regular.a8006b37.ttf"
+  },
+  {
+    "revision": "443a11034d097b723bfd13fd3a5a822f",
+    "url": "/static/media/logo.443a1103.png"
+  },
+  {
+    "revision": "0edb4027ecec1c1c181a1ad33e0ddce4",
+    "url": "/static/media/Banner.0edb4027.png"
+  },
+  {
+    "revision": "a8c4253b875ee527f03f0229f6fb5373",
+    "url": "/static/media/Card1.a8c4253b.png"
+  },
+  {
+    "revision": "0454ab89f3363171ae8a48ca4e97303e",
+    "url": "/static/media/Card2.0454ab89.png"
+  },
+  {
+    "revision": "9c61dad3188530491ce412907ec903c6",
+    "url": "/static/media/Card3.9c61dad3.png"
+  },
+  {
+    "revision": "b3f7c92eaec845b8b51460901c9cd270",
+    "url": "/static/media/Card4.b3f7c92e.png"
+  },
+  {
+    "revision": "d9f7323b04e47c8625006d920be14ca1",
+    "url": "/static/media/Benefits.d9f7323b.png"
+  },
+  {
+    "revision": "59e3670655b09605913aec25c741f321",
+    "url": "/static/media/beetlesWhite.59e36706.png"
+  },
+  {
+    "revision": "59a7f91927009aaaa959cab70b929c69",
+    "url": "/index.html"
+  }
+];
